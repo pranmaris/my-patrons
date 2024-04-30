2b5552691d21ac39fde2ed1540316786b83eec3d
Andrzej Precz <andrzej.precz@gmail.com>
2024-04-30 10:48:48 +0200

    challenge content block (stap 2 - functional)

--- resources/php/classes/ContentBlocks/ChallengesContentBlock.php
+++ resources/php/classes/ContentBlocks/ChallengesContentBlock.php
@@ -40,7 +40,9 @@ class ChallengesContentBlock extends ContentBlock implements ContentBlockInterfa
         ],
     ];
 
-    private $itemContent;
+    private $challengeContent;
+    private $checklistStepItemContent;
+    private $noteItemContent;
     private $fileData;
     private $textVariables;
 
@@ -48,13 +50,17 @@ class ChallengesContentBlock extends ContentBlock implements ContentBlockInterfa
     {
         $fileData = $this->getConsolidatedDataFilesArray($path);
 
-        $itemContent = $this->getOriginalHtmlFileContent('items/challenge-item.html');
+        $checklistStepItemContent = $this->getOriginalHtmlFileContent('items/challenge-checklist-step-item.html');
+        $noteItemContent = $this->getOriginalHtmlFileContent('items/challenge-note-item.html');
+        $challengeContent = $this->getOriginalHtmlFileContent('content-blocks/challenge-content-block.html');
 
         $translations = $this->getPreparedTranslations($fileData[self::MAIN_FILE_DATA_INDEX] ?? []);
         $language = $this->getLanguage();
         $textVariables = $this->getTranslatedVariablesForLangData($language, $translations);
 
-        $this->itemContent = $itemContent;
+        $this->challengeContent = $challengeContent;
+        $this->checklistStepItemContent = $checklistStepItemContent;
+        $this->noteItemContent = $noteItemContent;
         $this->fileData = $fileData;
         $this->textVariables = $textVariables;
 
@@ -64,20 +70,13 @@ class ChallengesContentBlock extends ContentBlock implements ContentBlockInterfa
     public function getFullContent(string $translatedName): string
     {
         $mainContent = $this->getOriginalHtmlFileContent('content-blocks/challenges-list-content-block.html');
-        $challengeContent = $this->getOriginalHtmlFileContent('content-blocks/challenge-content-block.html');
 
         $fileData = $this->fileData[self::MAIN_FILE_DATA_INDEX] ?? [];
         $textVariables = $this->textVariables;
 
         $challengeItemsContents = '';
         foreach ($fileData as $challengeType => $challengeData) {
-            $prefix = implode('-', [self::ROOT_TRANSLATION_PREFIX, mb_strtolower($challengeType)]);
-
-            $variables = [];
-            $variables['type'] = $challengeType;
-            $variables['name'] = self::VARIABLE_NAME_SIGN . $prefix . '-name' . self::VARIABLE_NAME_SIGN;
-            //...
-            $challengeItemsContents .= $this->getReplacedContent($challengeContent, $variables);
+            $challengeItemsContents .= $this->getChallengeContent($challengeType, $challengeData);
         }
 
         $variables = [];
@@ -89,9 +88,7 @@ class ChallengesContentBlock extends ContentBlock implements ContentBlockInterfa
 
     public function getRecordContent(string $recordId): string
     {
-        $itemContent = $this->itemContent;
-
-        return $itemContent;
+        //noop
     }
 
     private function getPreparedTranslations(
@@ -116,5 +113,60 @@ class ChallengesContentBlock extends ContentBlock implements ContentBlockInterfa
         }
 
         return $result;
+    }
+
+    private function getChallengeContent(string $challengeType, array $challengeData): string
+    {
+        $content = $this->challengeContent;
+
+        $prefix = implode('-', [self::ROOT_TRANSLATION_PREFIX, mb_strtolower($challengeType)]);
+
+        $variables = [];
+        $variables['element-id'] = $prefix;
+        $variables['type'] = $challengeType;
+        $variables['name'] = self::VARIABLE_NAME_SIGN . implode('-', [$prefix, self::FIELD_NAME]) . self::VARIABLE_NAME_SIGN;
+        $variables['desc'] = self::VARIABLE_NAME_SIGN . implode('-', [$prefix, self::FIELD_DESCRIPTION, self::FIELD_TEMPLATE]) . self::VARIABLE_NAME_SIGN;
+        $variables['person-desc'] = self::VARIABLE_NAME_SIGN . implode('-', [$prefix, self::FIELD_PERSON, self::FIELD_DESCRIPTION, self::FIELD_TEMPLATE]) . self::VARIABLE_NAME_SIGN;
+
+        $checklistStepsContents = '';
+        foreach ($challengeData[self::FIELD_CHECKLIST] ?? [] as $checklistStepId => $checklistStepData) {
+            $checklistStepsContents .= $this->getChecklistStepContent($prefix, $checklistStepId, $checklistStepData);
+        }
+        $variables['checklist-steps'] = $checklistStepsContents;
+
+        $notesContents = '';
+        foreach ($challengeData[self::FIELD_NOTES] ?? [] as $noteId => $noteData) {
+            $notesContents .= $this->getNoteContent($prefix, $noteId, $noteData);
+        }
+        $variables['notes'] = $notesContents;
+
+        return $this->getReplacedContent($content, $variables);
+    }
+
+    private function getChecklistStepContent(string $langVarPrefix, string $checklistStepId, array $checklistStepData): string
+    {
+        $content = $this->checklistStepItemContent;
+
+        $prefix = implode('-', [$langVarPrefix, self::FIELD_CHECKLIST, $checklistStepId]);
+
+        $variables = [];
+        $variables['checklist-step-id'] = $checklistStepId;
+        $variables['name'] = self::VARIABLE_NAME_SIGN . implode('-', [$prefix, self::FIELD_NAME]) . self::MODIFIER_SEPARATOR . self::MODIFIER_CAPITALIZE . self::VARIABLE_NAME_SIGN;
+        $variables['desc'] = self::VARIABLE_NAME_SIGN . implode('-', [$prefix, self::FIELD_DESCRIPTION, self::FIELD_TEMPLATE]) . self::VARIABLE_NAME_SIGN;
+
+        return $this->getReplacedContent($content, $variables);
+    }
+
+    private function getNoteContent(string $langVarPrefix, string $noteId, array $noteData): string
+    {
+        $content = $this->noteItemContent;
+
+        $prefix = implode('-', [$langVarPrefix, self::FIELD_NOTES, $noteId]);
+
+        $variables = [];
+        $variables['note-id'] = $noteId;
+        $variables['name'] = self::VARIABLE_NAME_SIGN . implode('-', [$prefix, self::FIELD_NAME]) . self::MODIFIER_SEPARATOR . self::MODIFIER_CAPITALIZE . self::VARIABLE_NAME_SIGN;
+
+        return $this->getReplacedContent($content, $variables);
     }
 }
